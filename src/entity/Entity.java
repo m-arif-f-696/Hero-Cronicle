330db56476d721e67e97f03cc081465b8439aef4
Terry Davina <terrydavina@mail.ugm.ac.id>
2026-05-13 13:15:48 +0700

    feat:tambah speed di entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -5,18 +5,21 @@ public abstract class Entity {
     protected String tipe;
     protected double hp;
     protected double maxHp;
+    protected double speed;
 
-    public Entity(String nama, String tipe, double hp) {
+    public Entity(String nama, String tipe, double hp, double speed) {
         this.nama  = nama;
         this.tipe  = tipe;
         this.hp    = hp;
         this.maxHp = hp;
+        this.speed = speed;
     }
 
     public String getNama()          { return nama; }
     public String getTipe()          { return tipe; }
     public double getHp()            { return hp; }
     public double getMaxHp()         { return maxHp; }
+    public double getSpeed()         { return speed; }
     public void   setHp(double hp)   { this.hp = Math.max(0, hp); }
     public boolean isAlive()         { return hp > 0; }
 
